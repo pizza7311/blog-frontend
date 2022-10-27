4b0ff7e120ab8ef5fa9dd43ce8aaa098d3b9e3a2
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2022-10-27 21:25:30 +0900

    add adsense code

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7661725827368330"
+     crossorigin="anonymous"></script>
+        </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
